05fc20b6355d62e5ce91ec71224a638f798479b4
SimonYung <simonquere24@gmail.com>
2020-04-20 17:36:34 +0200

    updated gallery next and previous button

--- assets/js/modal_gallery.js
+++ assets/js/modal_gallery.js
@@ -2,18 +2,45 @@
 // Get the modal gallery by id
 
 var modal = document.getElementById("ModalGallery");
-var img = 0;
-// boooooobs
+var img = -1;
+var currentImage = -1;
+var modalCaption = document.getElementById("Caption");
+
+// Modal
 
 function openModalGallery(imageIDNumber) {
+  currentImage = imageIDNumber;
   img = document.getElementById(imageIDNumber);
   if ( img == null ) {img = document.getElementById(-1);}
   var modalImg = document.getElementById("ImgModal");
   var captionText = document.getElementById("Caption");
-  modal.style.display = "block";
+  modal.style.display = "flex";
   modalImg.src = img.src;
   captionText.innerHTML = img.alt;
 }
+
+//prev and next
+
+
+function nextModal(n) {
+  currentImage += n;
+  img = document.getElementById(currentImage);
+  if ( img == null ) { currentImage -= n; img = document.getElementById(currentImage);}
+  var modalImg = document.getElementById("ImgModal");
+  var captionText = document.getElementById("Caption");
+  modal.style.display = "flex";
+  modalImg.src = img.src;
+  captionText.innerHTML = img.alt;
+}
+
+// toggle caption
+function toggleInfo() {
+  if (modalCaption.style.display != "block") 
+    { modalCaption.style.display = "block";}
+  else
+    { modalCaption.style.display = "none";}
+}
+
 	
 
 // Get the element that closes the modal
@@ -22,12 +49,3 @@ function closeModalGallery() {
 
   modal.style.display = "none";
 }
-//var closeButton = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-
-//closeButton.onclick = function() { 
-//  modal.style.display = "none";
-//  img = 0;
-//}
-
